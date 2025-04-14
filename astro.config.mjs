2907ec6df09079107854034a98e6af0d78df1731
Damian Lineiro <delineiro@gmail.com>
2025-04-14 01:25:56 -0300

    tailwind install

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,9 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
-
-import netlify from '@astrojs/netlify';
-
+import { defineConfig } from "astro/config";
+import tailwindcss from "@tailwindcss/vite";
 // https://astro.build/config
 export default defineConfig({
-  adapter: netlify()
+  vite: {
+    plugins: [tailwindcss()],
+  },
 });
